dc7cc21df5acf2cc242263d6c7c89640fa5083d5
arryansh5 <arryanshmesson11@gmail.com>
2026-07-13 23:23:24 +0530

    Striver sheet

---
+++ Greedy Algo/leetcode_435.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size() ; 
+        sort(intervals.begin() , intervals.end() , [](vector<int>& a , vector<int>& b){
+            return a[1] < b[1]; 
+        });
+
+        int removed = 0 ; 
+        int lastend = intervals[0][1]; 
+
+        for(int i = 1 ; i < n ; i++){
+            if(intervals[i][0] < lastend) removed++; 
+            else lastend = intervals[i][1]; 
+        }
+
+        return removed;
+    }
+};
