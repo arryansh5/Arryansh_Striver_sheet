2b009237127c4e51351f7cbde0056d70f591423c
arryansh5 <arryanshmesson11@gmail.com>
2026-07-14 23:58:34 +0530

    Greedy Algo

---
+++ Greedy Algo/leetcode_11.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size(); 
+        int i = 0 , j = n -1 , currarea = 0 ,maxarea = 0; 
+        while(i < j){
+            currarea = ((j-i)*(min(height[i] , height[j]))); 
+            (height[i] <= height[j]) ?  i++ : j--; 
+            maxarea = max(currarea , maxarea); 
+        }
+        return maxarea ; 
+    }
+};
